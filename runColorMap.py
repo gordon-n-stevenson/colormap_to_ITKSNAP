1ef8508790071b8d19b0da5e3dde1f48eb9e9479
Gordon Stevenson <gordon.n.stevenson@gmail.com>
2016-08-17 13:46:44 +1000

    add numpy

--- runColorMap.py
+++ runColorMap.py
@@ -1,5 +1,7 @@
 import matplotlib.cm as cmap
+import numpy as np
 import ColorMapWriter
+
 mpl_map = cmap.viridis(np.arange(256))
 c = ColorMapWriter.ColorMapWriter(mpl_map, 'viridis.xml', 5)
 c.createLookupTable()
